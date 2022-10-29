6f98e4e1e8168ad5b11c47586d5bc0851dfc8768
ullasasindhur <poojithullasa@gmail.com>
2022-10-29 22:03:23 +0530

    Added variables

---
+++ 02_variables/go.mod
@@ -0,0 +1,3 @@
+module github.com/ullasasindhur/go-learning/tree/main/02_variables
+
+go 1.19
---
+++ 02_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var name = "poo"
+
+func main() {
+	fmt.Println("Variables")
+	var username string = "Ullasa Sindhur"
+	fmt.Println(username, name)
+	fmt.Printf("Variable is of type: %T \n", username)
+	var name = "poojith"
+	fmt.Println(name)
+	pet_name := "sindhur"
+	fmt.Println(pet_name)
+}
